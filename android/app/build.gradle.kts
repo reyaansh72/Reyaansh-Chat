9989b5b520b1443a93cd4fbaa588f48f6d0b911a
reyaansh72 <bhavya150520@gmail.com>
2026-07-25 12:21:11 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
