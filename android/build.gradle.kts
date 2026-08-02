5334fb7687f249a342279fb1ffd46aaf7a084a57
reyaansh72 <bhavya150520@gmail.com>
2026-08-02 13:58:37 +0530

    android: set compileSdk to 36 and force compileSdkVersion=36 for all Android subprojects to satisfy plugin AAR metadata checks

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,16 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+// Ensure all Android subprojects use a minimum compileSdk of 36 so
+// plugins compiled with older compileSdk (e.g., android-34) won't
+// fail AAR metadata checks when other plugins require 36+.
+import com.android.build.gradle.BaseExtension
+
+subprojects {
+    afterEvaluate {
+        extensions.findByType(BaseExtension::class.java)?.apply {
+            compileSdkVersion(36)
+        }
+    }
+}
